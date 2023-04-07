c77aa6f0fdc8adddeb74b1b7c4859d65dc5764c0
sakshipatil19 <100662966+sakshipatil19@users.noreply.github.com>
2023-04-07 12:12:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-# with open("README.md", "r", encoding="utf-8") as f:
-#     long_description = f.read()
-
 ## edit below variables as per your requirements -
 REPO_NAME = "Movie Recommendation System"
 AUTHOR_USER_NAME = "Sakshi"
@@ -17,8 +14,8 @@ setup(
     description="A small package for Movie Recommender System",
     # long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="entbappy73@gmail.com",
+    url=f"https://github.com/sakshipatil19/Movie-Recommendation-System",
+    author_email="abc@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.7",
